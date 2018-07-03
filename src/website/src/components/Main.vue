04e756c0d36dcc2e37ef59cfe978ba52d198e0b8
while <while@while.com>
2018-07-03 20:49:59 +0800

    update

---
+++ src/website/src/components/Main.vue
@@ -0,0 +1,47 @@
+<template>
+    <el-container style="height: 100%; border: 1px solid #eee">
+        <el-header>
+            <el-menu :default-active="activeIndex"
+                :center = true
+                background-color="white"
+                text-color="black"
+                active-text-color="black"
+                mode="horizontal" 
+                @select="handleSelect">
+                <el-menu-item index="index">主页</el-menu-item>
+                <el-submenu index="project">
+                    <template slot="title">项目</template>
+                    <el-menu-item v-for="item of projects" :key="item" :index="item">
+                        {{ item }}
+                    </el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-header>
+        <el-main>
+            <IndexPart v-show="activeIndex == 'index'"></IndexPart>
+        </el-main>
+        <el-footer>Footer</el-footer>
+    </el-container>
+</template>
+<script>
+    import IndexPart from './IndexPart';
+    export default {
+        data() {
+            return {
+                projects: ["1111","22222"],
+                activeIndex: 'index',
+            }
+        },
+        methods : {
+            handleSelect : function(index) {
+                console.log("==========1111111111111111 " + index)
+            }
+        }
+    }
+</script>
+<style>
+    html, body {
+      height: 100%;
+      margin: 0;
+    }
+</style>
--- src/website/src/router/index.js
+++ src/website/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/components/Index'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Index',
-      component: Index
+      name: 'Main',
+      component: Main
     }
   ]
 })
