e29cb49aa73571505bd1204c89aea8e0445247aa
linyuan.yang <linyuan.yang@centurygame.com>
2019-07-09 12:06:20 +0800

    安装脚本

---
+++ installsco.ps1
@@ -0,0 +1,6 @@
+﻿$str = ((New-Object System.Net.WebClient).DownloadString('https://qingfeng346.github.io/install/common.ps1'))
+$fields = '$projectName="Scorpio-CSharp"
+$applicationName="sco"
+'
+Write-Host $fields
+Invoke-Expression "$fields $str"
