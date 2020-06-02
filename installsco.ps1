8fea1372ad51303c4b73c3b93d9fe9a48335bf96
linyuan.yang <linyuan.yang@centurygame.com>
2020-06-02 18:05:12 +0800

    remove

--- installsco.ps1
+++
@@ -1,6 +0,0 @@
-﻿$str = ((New-Object System.Net.WebClient).DownloadString('https://qingfeng346.github.io/install/common.ps1'))
-$fields = '$projectName="Scorpio-CSharp"
-$applicationName="sco"
-'
-Write-Host $fields
-Invoke-Expression "$fields $str"
